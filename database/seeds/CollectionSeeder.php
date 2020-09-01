1246e6824a2eea9b98b42ff68c182e9952454747
amirmojiry <amojiry@gmail.com>
2020-09-01 11:19:36 +0430

    update collection seeder (use eloquent instead of DB query)

--- database/seeds/CollectionSeeder.php
+++ database/seeds/CollectionSeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
-use Illuminate\Support\Facades\DB;
+use App\Collection;
 
 class CollectionSeeder extends Seeder
 {
@@ -13,10 +13,76 @@ class CollectionSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('collection')->insert([
-            'title' => '',
-            'title_abbr' => '',
-            'intro' => '',
-        ]);
+        $collection = new Collection;
+        $collection->title = 'مینا';
+        $collection->title_abbr = 'mina';
+        $collection->intro = '';
+        $collection->save();
+
+        $collection = new Collection;
+        $collection->title = 'لذت خواندن';
+        $collection->title_abbr = 'enjoyreading';
+        $collection->intro = '';
+        $collection->save();
+
+        $collection = new Collection;
+        $collection->title = 'ایران‌شناسی';
+        $collection->title_abbr = 'iranology';
+        $collection->intro = '';
+        $collection->save();
+
+        $collection = new Collection;
+        $collection->title = 'گام اول';
+        $collection->title_abbr = '1ststep';
+        $collection->intro = '';
+        $collection->save();
+
+        $collection = new Collection;
+        $collection->title = 'چارچوب';
+        $collection->title_abbr = 'persianframe';
+        $collection->intro = '';
+        $collection->save();
+
+        $collection = new Collection;
+        $collection->title = 'آموزش کاربردی واژه';
+        $collection->title_abbr = 'persianinuse';
+        $collection->intro = '';
+        $collection->save();
+
+        $collection = new Collection;
+        $collection->title = 'شیراز';
+        $collection->title_abbr = 'shiraz';
+        $collection->intro = '';
+        $collection->save();
+
+        $collection = new Collection;
+        $collection->title = 'استاندارد';
+        $collection->title_abbr = 'persianstandard';
+        $collection->intro = '';
+        $collection->save();
+
+        $collection = new Collection;
+        $collection->title = 'سفر به ایران';
+        $collection->title_abbr = 'trip2iran';
+        $collection->intro = '';
+        $collection->save();
+
+        $collection = new Collection;
+        $collection->title = 'روش‌های سادۀ آموزش زبان فارسی';
+        $collection->title_abbr = 'easyteaching';
+        $collection->intro = '';
+        $collection->save();
+
+        $collection = new Collection;
+        $collection->title = 'خمره';
+        $collection->title_abbr = 'khomreh';
+        $collection->intro = '';
+        $collection->save();
+
+        $collection = new Collection;
+        $collection->title = 'سلام دکتر';
+        $collection->title_abbr = 'hidr';
+        $collection->intro = '';
+        $collection->save();
     }
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(LevelSeeder::class);
+        $this->call(CollectionSeeder::class);
     }
 }
