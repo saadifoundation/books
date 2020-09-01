a1beac9e104b246509cb5b831e253e6486f39417
amirmojiry <amojiry@gmail.com>
2020-09-01 14:20:10 +0430

    add name english to users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('name_en');
             $table->string('email')->unique();
             $table->text('intro')->nullable();
             $table->string('pic')->nullable();
