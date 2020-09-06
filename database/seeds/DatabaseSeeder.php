aba264265e9519d15b5abd4753d5264fb18175ab
amirmojiry <amojiry@gmail.com>
2020-09-06 13:13:25 +0430

    add role seeder to database seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call(LevelSeeder::class);
         $this->call(CollectionSeeder::class);
         $this->call(UserSeeder::class);
+        $this->call(RoleSeeder::class);
         $this->call(BookSeeder::class);
     }
 }
