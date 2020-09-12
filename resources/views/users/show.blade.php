5fd7483de34fe8fe8a9b8d33a374baeb571c243a
amirmojiry <amojiry@gmail.com>
2020-09-12 13:00:45 +0430

    add default ocver on views

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -60,7 +60,7 @@
                 <div class="col-6 col-md-3 mb-2">
                     <a href="{{ route('books.show', $book) }}">
                     <div class="card book-card">
-                        <img src="{{ Storage::url($book->cover) }}" class="card-img-top">
+                        <img src="@if($book->cover !== NULL) {{ Storage::url($book->cover) }} @else {{ asset('img/cover.jpg') }} @endif" class="card-img-top">
                         <div class="card-body">
                             <p class="card-text">
                                 {{$book->title}}
