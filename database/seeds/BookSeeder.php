9d004d266f8e36832470a4691f8244f8d3b1bdb4
amirmojiry <amojiry@gmail.com>
2020-09-12 12:58:14 +0430

    add mina3 to book seeder

--- database/seeds/BookSeeder.php
+++ database/seeds/BookSeeder.php
@@ -96,6 +96,43 @@ class BookSeeder extends Seeder
                     ],
                 ],
             ],
+            [
+                'title' => 'مینا 3',
+                'title_abbr' => 'mina3',
+                'cover' => 'covers/mina3.jpg',
+                'collection_id' => Collection::firstWhere('title_abbr', 'mina')->id,
+                'intro' => <<<INTRO
+                اگر می‌خواهید زبانِ فارسیِ معیار را به شیوه‌ای لذّت‌بخش فرابگیرید، مینا بهترین انتخاب است. طرّاحیِ جذّاب مینا و محتوای کاربردی و حساب‌شده آن، به شما کمک می‌کند واژه‌ها، کارکردها و ساخت‌های دستوریِ مهّمِ زبانِ فارسی را، به‌سادگی و به‌سرعت، بیاموزید. درس‌های مینا، به گونه‌ای طرّاحی شده‌اند که هم فارسیِ گفتاری را به شما می‌آموزند و هم فارسیِ نوشتاری را. با مینا می‌توانید خیلی زود، به زبانِ فارسی صحبت کنید و بنویسید.
+                مجموعه مینا در چارچوب الگوهای نوینِ آموزشِ زبانِ دوّم/خارجی تدوین شده‌است. در این مجموعه، همه مهارت‌ها و مؤلفه‌های زبان فارسی مورد توّجه بوده‌اند. مینا در زبان و ادبیات فارسی به معنای مرغِ سخنگو است؛ یعنی با خواندنِ مینا شما می‌توانید در آسمانِ آبیِ زبانِ فارسی پروازی شوق‌انگیز داشته‌باشید!
+                مینا یک مجموعه 4 جلدی است و هر جلد آن هشت درس دارد. درس‌های مینا ساختارِ مشابهی دارند و هم‌زمان همه مهارت‌های زبانی را مورد توجّه قرار داده‌اند. محتوای هر درس، شامل دو ‌مکالمه کاربردی، دو کارکرد ارتباطی، دو ساختِ دستوریِ مرتبط و دامنه حساب‌شده‌ای از واژه‌های مهم است. به‌علاوه، در هر درس، بخشی به مهارتِ خواندن اختصاص یافته و در همه تمرین‌ها به مهارتِ نوشتن نیز توجّه‌شده‌است. مینا، به نقشِ بازی و تکلیف‌های خلّاق توجّه داشته و صفحه پایانیِ همه درس را به این گونه فعّالیت‌ها اختصاص‌داده‌است.
+                مینا مجموعه‌ای است چند‌رسانه‌ای، که امکان می‌دهد حجم قابل‌توجّهی از گفتارِ روزمره فارسی‌زبانان را در بافت‌ها و موقعیت‌های گوناگون بشنوید.
+                INTRO,
+                'intro_video' => null,
+                'teaching_video' => null,
+                'elearning_link' => 'https://mina.saadifoundation.ir',
+                'bibliography_link' => 'http://opac.nlai.ir/opac-prod/bibliographic/6221281',
+                'index_file' => null,
+                'sample_file' => null,
+                'buying_link' => null,
+                'ebuying_link' => null,
+                'audio_link' => 'audio_zip/mina2.zip',
+                'levels' => [
+                    Level::firstWhere('title_abbr', 'b2'),
+                    Level::firstWhere('title_abbr', 'b3'),
+                ],
+                'writer_users' => [
+                    User::firstWhere('name_en', 'Reza-Sahraee'),
+                    User::firstWhere('name_en', 'Afsaneh-Gharibi'),
+                    User::firstWhere('name_en', 'Monire-Shahbaz'),
+                    User::firstWhere('name_en', 'Maryam-Soltani'),
+                ],
+                'other_users' => [
+                    [
+                        'model' => User::firstWhere('name_en', 'Mohammd-Mehraabi'),
+                        'role_title' => Role::firstWhere('title_abbr', 'graphist'),
+                    ],
+                ],
+            ],
         ];
         foreach ($books_data as $book_data) {
             $book = new Book;
