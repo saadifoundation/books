a66f0d46e0c2d9bea6d90be3a0468d89cb7828e0
amirmojiry <amojiry@gmail.com>
2020-09-12 11:20:56 +0430

    add default person pic on book show

--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -215,7 +215,7 @@
         <div class="col-6 col-md-3 person">
           <a href="{{ route('users.show', $user->name_en) }}">
             <figure class="figure text-center">
-              <img src='{{ Storage::url("$user->pic") }}' alt="" class="w-50 rounded figure-img img-fluid">
+              <img src="@if($user->pic !== '') {{Storage::url($user->pic)}} @else {{asset('/img/person.jpg')}} @endif" alt="" class="w-50 rounded figure-img img-fluid">
               <figcaption class="figure-caption text-center">
                 {{ $user->name }}
                 <span class="badge badge-primary">
@@ -247,7 +247,7 @@
           <div class="col-6 col-md-3 person other-person">
             <a href="{{ route('users.show', $user->name_en) }}">
               <figure class="figure text-center">
-                <img src='{{ Storage::url("$user->pic") }}' alt="" class="w-50 rounded figure-img img-fluid">
+                <img src="@if($user->pic !== '') {{Storage::url($user->pic)}} @else '{{asset('/img/person.jpg')}}' @endif" alt="" class="w-50 rounded figure-img img-fluid">
                 <figcaption class="figure-caption text-center">
                   {{ $user->name }}
                   <span class="badge badge-primary">
@@ -275,7 +275,7 @@
             <img src="{{ Storage::url($book->cover) }}" class="card-img-top">
             <div class="card-body">
               <p class="card-text">
-                $book->title
+                {{$book->title}}
               </p>
             </div>
           </div>
