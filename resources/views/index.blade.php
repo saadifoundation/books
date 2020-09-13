d9e1d170f0063da91efd7efcf653331168814b46
amirmojiry <amojiry@gmail.com>
2020-09-13 12:02:39 +0430

    add ifs for when db is empty

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -29,112 +29,118 @@
         </div>
       </div>
     </div>
-    <div class="row text-center border-top mb-3" id="levels-row">
-      <div class="col-12">
-          <h2 class="mb-4 mt-4">
-            سطوح استاندارد مرجع بنیاد سعدی
-          </h2>
-          <p>
-              کتاب‌های بنیاد سعدی در 7 سطح مختلف تألیف و منتشر می‌شوند. این سطح‌بندی بر اساس
-              <a href="mina2.html">
-                  استاندارد مرجع آموزش زبان فارسی
-              </a>
-              است.
-          </p>
-      </div>
-      <div class="col-12">
-          <div class="levels">
-              @foreach ($levels as $level)
-                  <div class="mb-1">
-                      <a class="progress" href="{{route('level', $level)}}">
-                          <div class="progress-bar progress-bar-striped progress-bar-animated bg-level-{{$level->title_abbr}}" role="progressbar" aria-valuenow="{{$level->width}}" 
-                          aria-valuemin="0" aria-valuemax="100" style="width: {{$level->width}}%">
-                              {{ __("$level->title") }}: {{ strtoupper($level->title_abbr) }}
-                          </div>
-                      </a>
-                  </div> 
-              @endforeach
-          </div>
-      </div>
-    </div>
-    <div class="row text-center">
-      <div class="col-12">
-          <h2 class="mb-4 mt-4">
-            {{ __('جدول کتاب‌های بنیاد سعدی') }}
-          </h2>
-      </div>
-      <div class="col-12">
-          <div class="table-responsive">
-              <table class="table table-hover table-bordered">
-                  <thead>
-                      <tr>
-                          <th scope="col">
-                              {{ __('سطح') }}
-                          </th>
-                          <th scope="col">
-                              {{ __('کتاب‌ها') }}
-                          </th>
-                      </tr>
-                  </thead>
-                  <tbody>
-                      @foreach($levels as $level)
-                          <tr>
-                              <th scope="row">
-                                  <div class="mb-1">
-                                      <a class="progress" href="{{route('level', $level)}}">
-                                          <div class="progress-bar progress-bar-striped progress-bar-animated bg-level-{{$level->title_abbr}}" role="progressbar" aria-valuenow="{{$level->width}}" 
-                                          aria-valuemin="0" aria-valuemax="100" style="width: {{$level->width}}%">
-                                          {{ strtoupper($level->title_abbr)}}
-                                          </div>
-                                      </a>
-                                      <p>
-                                          {{ __($level->title) }}
-                                      </p>
-                                  </div>
-                              </th>
-                              <td>
-                                @foreach ($level->books as $book)
-                                  <a href="{{ route('books.show', $book->title_abbr) }}">
-                                    <img src="{{ $book->cover !== null ? Storage::url($book->cover) : asset('img/cover.jpg') }}" alt="{{ $book->title }}" class="w-25 mb-3">
-                                  </a>
-                                @endforeach
-                              </td>
-                          </tr>
-                      @endforeach
-                  </tbody>
-              </table>
-          </div>
-      </div>
-    </div>
-    <div class="row text-center d-flex justify-content-center border-top mb-4" id="books-row">
-    <div class="col-12">
-      <h2 class="mb-4 mt-4">
-          بعضی از کتاب‌های بنیاد سعدی
-      </h2>
-    </div>
-    @foreach ($books->random(3) as $book)
-      <div class="col-6 col-md-3 mb-2">
-        <a href="{{route('books.show', $book->title_abbr)}}">
-          <div class="card book-card">
-            <img src="{{ $book->cover !== null ? Storage::url($book->cover) : asset('img/cover.jpg') }}" class="card-img-top">
-            <div class="card-body">
-              <p class="card-text">
-                {{ $book->title }}
-                @foreach ($book->levels as $level)
-                  <span class="badge badge-primary">
-                    {{ $level->title }}
-                  </span>
+    @if($levels->isNotEmpty())
+      <div class="row text-center border-top mb-3" id="levels-row">
+        <div class="col-12">
+            <h2 class="mb-4 mt-4">
+              سطوح استاندارد مرجع بنیاد سعدی
+            </h2>
+            <p>
+                کتاب‌های بنیاد سعدی در 7 سطح مختلف تألیف و منتشر می‌شوند. این سطح‌بندی بر اساس
+                <a href="mina2.html">
+                    استاندارد مرجع آموزش زبان فارسی
+                </a>
+                است.
+            </p>
+        </div>
+        <div class="col-12">
+            <div class="levels">
+                @foreach ($levels as $level)
+                    <div class="mb-1">
+                        <a class="progress" href="{{route('level', $level)}}">
+                            <div class="progress-bar progress-bar-striped progress-bar-animated bg-level-{{$level->title_abbr}}" role="progressbar" aria-valuenow="{{$level->width}}" 
+                            aria-valuemin="0" aria-valuemax="100" style="width: {{$level->width}}%">
+                                {{ __("$level->title") }}: {{ strtoupper($level->title_abbr) }}
+                            </div>
+                        </a>
+                    </div> 
                 @endforeach
-              </p>
             </div>
-          </div>
-        </a>
+        </div>
       </div>
-    @endforeach
-    <div class="col-12">
-        <a class="btn btn-primary btn-block" href="books.html">
-            همۀ کتاب‌ها
-          </a>
-    </div>
-  </div>
+    @endif
+    @if($books->isNotEmpty() && $levels->isNotEmpty())
+      <div class="row text-center">
+        <div class="col-12">
+            <h2 class="mb-4 mt-4">
+              {{ __('جدول کتاب‌های بنیاد سعدی') }}
+            </h2>
+        </div>
+        <div class="col-12">
+            <div class="table-responsive">
+                <table class="table table-hover table-bordered">
+                    <thead>
+                        <tr>
+                            <th scope="col">
+                                {{ __('سطح') }}
+                            </th>
+                            <th scope="col">
+                                {{ __('کتاب‌ها') }}
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($levels as $level)
+                            <tr>
+                                <th scope="row">
+                                    <div class="mb-1">
+                                        <a class="progress" href="{{route('level', $level)}}">
+                                            <div class="progress-bar progress-bar-striped progress-bar-animated bg-level-{{$level->title_abbr}}" role="progressbar" aria-valuenow="{{$level->width}}" 
+                                            aria-valuemin="0" aria-valuemax="100" style="width: {{$level->width}}%">
+                                            {{ strtoupper($level->title_abbr)}}
+                                            </div>
+                                        </a>
+                                        <p>
+                                            {{ __($level->title) }}
+                                        </p>
+                                    </div>
+                                </th>
+                                <td>
+                                  @foreach ($level->books as $book)
+                                    <a href="{{ route('books.show', $book->title_abbr) }}">
+                                      <img src="{{ $book->cover !== null ? Storage::url($book->cover) : asset('img/cover.jpg') }}" alt="{{ $book->title }}" class="w-25 mb-3">
+                                    </a>
+                                  @endforeach
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+      </div>
+    @endif
+    @if($books->isNotEmpty())
+      <div class="row text-center d-flex justify-content-center border-top mb-4" id="books-row">
+        <div class="col-12">
+          <h2 class="mb-4 mt-4">
+              بعضی از کتاب‌های بنیاد سعدی
+          </h2>
+        </div>
+        @foreach ($books->random(3) as $book)
+          <div class="col-6 col-md-3 mb-2">
+            <a href="{{route('books.show', $book->title_abbr)}}">
+              <div class="card book-card">
+                <img src="{{ $book->cover !== null ? Storage::url($book->cover) : asset('img/cover.jpg') }}" class="card-img-top">
+                <div class="card-body">
+                  <p class="card-text">
+                    {{ $book->title }}
+                    @foreach ($book->levels as $level)
+                      <span class="badge badge-primary">
+                        {{ $level->title }}
+                      </span>
+                    @endforeach
+                  </p>
+                </div>
+              </div>
+            </a>
+          </div>
+        @endforeach
+        <div class="col-12">
+            <a class="btn btn-primary btn-block" href="books.html">
+                همۀ کتاب‌ها
+              </a>
+        </div>
+      </div>
+    @endif
 @endsection
