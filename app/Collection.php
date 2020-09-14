a08d9ca774ebd67337bea19da01091bbe59c06e4
amirmojiry <amojiry@gmail.com>
2020-09-14 12:36:33 +0430

    add title_abbr as collection key

--- app/Collection.php
+++ app/Collection.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Collection extends Model
 {
+    public function getRouteKeyName()
+    {
+        return 'title_abbr';
+    }
     public function books()
     {
         return $this->hasMany('App\Book');
