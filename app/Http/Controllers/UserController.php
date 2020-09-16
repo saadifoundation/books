68cb7d00a94c0f5ea350a6589b5571ca5d54ba3a
amirmojiry <amojiry@gmail.com>
2020-09-16 12:12:37 +0430

    add users index

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,7 +15,10 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+        $users = User::all();
+        return view('users.index', [
+            'users' => $users,
+        ]);
     }
 
     /**
---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.app')
+
+@section('title', __('همۀ همکاران بنیاد سعدی'))
+
+@section('content')
+
+<div class="row">
+    <div class="col-12">
+        <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="{{route('index') }}">
+            {{ __('صفحۀ اصلی') }}
+            </a></li>
+            <li class="breadcrumb-item active" aria-current="page">
+            {{ __('همکاران') }}
+            </li>
+        </ol>
+        </nav>
+    </div>
+</div>
+<div class="row mb-4 text-center d-flex justify-content-center">
+    <div class="col-12">
+        <div class="description font-weight-light">
+            <p>
+            در مجموع تاکنون
+            <span class="badge">{{ $users->count() }}</span>
+            فرد با بنیاد سعدی همکاری کرده‌اند.
+            </p>
+        </div>
+    </div>
+    @foreach ($users->unique() as $user)
+        <div class="col-6 col-md-3 person">
+            <a href="{{ route('users.show', $user) }}">
+            <figure class="figure text-center">
+                <img src="{{ $user->pic !== NULL ? Storage::url($user->pic) : asset('/img/person.jpg') }}" alt="{{ $user->name }}" class="w-50 rounded figure-img img-fluid">
+                <figcaption class="figure-caption text-center">
+                {{ $user->name }}
+                </figcaption>
+            </figure>
+            </a>
+        </div>
+    @endforeach
+</div>
+@endsection
