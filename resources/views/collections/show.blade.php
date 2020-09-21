18026b5159317c0fcec5d9d693c6e97b298c1dd8
amirmojiry <amojiry@gmail.com>
2020-09-21 09:35:40 +0330

    add link to levels on collection show

--- resources/views/collections/show.blade.php
+++ resources/views/collections/show.blade.php
@@ -43,7 +43,7 @@
                 <div class="card-body">
                   @foreach ($book->levels as $level)
                     <div class="mb-1">
-                      <a class="progress" href="#">
+                      <a class="progress" href="{{ route('levels.show', $level) }}">
                         <div class="progress-bar progress-bar-striped progress-bar-animated bg-level-{{$level->title_abbr}}" role="progressbar" aria-valuenow="{{$level->width}}" 
                         aria-valuemin="0" aria-valuemax="100" style="width: {{$level->width}}%">
                           {{ strtoupper($level->title_abbr) }}
