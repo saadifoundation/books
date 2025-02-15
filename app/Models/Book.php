de64a201e217ec3f1d51ace6568ea0d86fc81676
Amir Mojiri <amojiry@gmail.com>
2025-02-15 11:43:24 +0200

    Add is active to book model

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Scopes\ActiveBookScope;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
@@ -9,6 +10,11 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Book extends Model
 {
+    protected static function booted()
+    {
+        static::addGlobalScope(new ActiveBookScope());
+    }
+
     public function getRouteKeyName(): string
     {
         return 'title_abbr';
